b0abe99811ce08f70930ce388479ae4b0c25af52
Rick van der Staaij <6495166+RickvdStaaij@users.noreply.github.com>
2018-02-26 16:47:38 +0100

    Improve roll-out output

--- wait-for-rollout.sh
+++ wait-for-rollout.sh
@@ -1,32 +1,42 @@
 #!/bin/sh
 
+echo "Waiting for a rollout of $2 in namespace $1...";
 COUNT=0
 STARTED=false
 while [ $STARTED = false ]; do
     COUNT=$((COUNT+1))
-    if [ $COUNT -ge 10 ]; then
-        echo "Could not find a rollout within $COUNT/10 tries, continuing..."
-        break
+    kubectl -n "$1" get pods | grep "$2" | grep -v Running > /dev/null && STARTED=true
+    if [ $STARTED = false ]; then
+        if [ $COUNT -ge 10 ]; then
+            echo "Could not find a rollout within 10 tries."
+            echo "Did it already finish? Moving on..."
+            break
+        fi
+        echo "Waiting for a roll-out start $COUNT/10..."
+        sleep 1
     fi
-    kubectl -n $1 get pods | grep $2 | grep -v Running > /dev/null && STARTED=true
-    echo "Waiting for container roll-out start $COUNT/10..."
-    sleep 1
 done
+
+if [ $STARTED = true ]; then
+    echo "Found a $2 roll-out, waiting for completion..."
+fi
 
 COUNT=0
 COMPLETED=false
 while [ $COMPLETED = false ]; do
     COUNT=$((COUNT+1))
-    if [ $COUNT -ge 120 ]; then
-        echo "Could not find a completed rollout within $COUNT/120 tries. Terminating."
-        exit 1
+    kubectl -n "$1" get pods | grep "$2"
+    kubectl -n "$1" get pods | grep "$2" | grep -v Running > /dev/null || COMPLETED=true
+    if [ $COMPLETED = false ]; then
+        if [ $COUNT -ge 120 ]; then
+            echo "Could not find a completed rollout within 120 tries. Terminating."
+            exit 1
+        fi
+        echo "Roll-out in progress ($COUNT/120)..."
+        sleep 1
     fi
-    echo "Waiting for the container roll-out to complete $COUNT/120..."
-    kubectl -n $1 get pods | grep $2
-    kubectl -n $1 get pods | grep $2 | grep -v Running > /dev/null || COMPLETED=true
-    sleep 1
 done
 
 echo "The container roll-out is complete, waiting 5 seconds to be extra sure...";
 sleep 5
-echo "Done!"
+echo "Done, $2 should be available!"
